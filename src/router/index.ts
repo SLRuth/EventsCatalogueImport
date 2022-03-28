92cf3ba34790037bc37f8af5d3fdea1095108987
Michael Nelo <syfers@gumi-os.gumi-os.localdomain>
2022-03-28 04:59:24 -0400

    Added nodenv, changed history mode to hash

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,21 +1,21 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
 
 const routes: Array<RouteRecordRaw> = [
-    {
-        path: '/',
-        name: 'Imports',
-        component: () => import('../views/Imports.vue')
-    },
-    {
-        path: '/entities',
-        name: 'Entities',
-        component: () => import('../views/Entities.vue')
-    },
-]
+  {
+    path: "/",
+    name: "Imports",
+    component: () => import("../views/Imports.vue"),
+  },
+  {
+    path: "/entities",
+    name: "Entities",
+    component: () => import("../views/Entities.vue"),
+  },
+];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  history: createWebHashHistory(),
+  routes,
+});
 
-export default router
+export default router;
